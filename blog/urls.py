4d355f0d128960caba001575152350972e1575ce
RENZOje <RENZOje@users.noreply.github.com>
2020-09-12 17:28:37 +0300

    add draft post/ last_time_update

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
 
     path('', MainView.as_view(), name='home'),
+    path('draft_post', DraftPostView.as_view(), name='draft_post'),
     path('tag/<str:slug>', TagPostView.as_view(), name='detail_tag'),
     path('post/<slug:slug>', PostDetailView.as_view(), name='detail_post'),
 
--- blog/views.py
+++ blog/views.py
@@ -5,14 +5,28 @@ from django.contrib.auth import authenticate, login, logout
 from .forms import *
 from django.contrib import messages
 from django.forms import inlineformset_factory
-
+from django.utils import timezone
 
 # Create your views here.
 
 class MainView(ListView):
     model = Post
     template_name = 'main.html'
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(draft=False)
+    context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['tags'] = Tag.objects.all()
+        return context
+
+
+class DraftPostView(ListView):
+    model = Post
+    template_name = 'main.html'
+    queryset = Post.objects.filter(draft=True)
     context_object_name = 'posts'
 
     def get_context_data(self, **kwargs):
@@ -26,7 +40,7 @@ class MainView(ListView):
 class TagPostView(View):
     def get(self, request, slug):
         tag = Tag.objects.get(link=slug)
-        posts = Post.objects.filter(tags=tag)
+        posts = Post.objects.filter(tags=tag, draft=False)
 
         context = {'posts': posts, 'tag': tag}
 
@@ -62,10 +76,11 @@ def update_post(request, pk):
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
+            form.cleaned_data['time_last_update'] = timezone.now()
             form.save()
             return redirect('/')
 
-    context = {'form': form}
+    context = { 'form': form }
     return render(request, 'accounts/post_form.html', context)
 
 
